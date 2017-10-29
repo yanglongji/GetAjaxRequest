9d8ee1ea93dcec93c24b1f24f50c43647d6c40e4
yanglongji <yanglongji9@gmail.com>
2017-10-29 22:25:10 +0800

    add android

--- src/android/GetAjaxRequest.java
+++ src/android/GetAjaxRequest.java
@@ -1,9 +1,9 @@
-package com.jianshi;
+package com.jianshi.tools;
 
+import android.webkit.CookieManager;
+import android.webkit.CookieSyncManager;
 import org.apache.cordova.CallbackContext;
-import org.apache.cordova.CordovaInterface;
 import org.apache.cordova.CordovaPlugin;
-import org.apache.cordova.CordovaWebView;
 import org.json.JSONArray;
 import org.json.JSONException;
 
@@ -12,7 +12,18 @@ public class GetAjaxRequest extends CordovaPlugin{
     @Override
     public boolean execute(String action, JSONArray args, CallbackContext callbackContext) throws JSONException {
         if ("setCookie".equals(action)){
-          
+          String[] cookies = args.getString(0).split(";");
+          CookieManager cookieManager = CookieManager.getInstance();
+          cookieManager.setAcceptCookie(true);
+          for(int i = 0; i < cookies.length; i++) {
+            cookieManager.setCookie(args.getString(1), cookies[i]);
+          }
+          if (android.os.Build.VERSION.SDK_INT >= android.os.Build.VERSION_CODES.LOLLIPOP) {
+            cookieManager.flush();
+          } else {
+            CookieSyncManager.createInstance(this.cordova.getActivity());
+            CookieSyncManager.getInstance().sync();
+          }
         }
         return  false;
     }
