05352255bfde050d183eb869c0a8734ccac8d4e6
yanglongji <yanglongji9@gmail.com>
2017-11-07 13:28:45 +0800

    wechat login

--- src/android/GetAjaxRequest.java
+++ src/android/GetAjaxRequest.java
@@ -1,46 +1,46 @@
 package com.jianshi.tools;
 
-import android.util.Log;
-import android.webkit.CookieManager;
-import android.webkit.CookieSyncManager;
-import org.apache.cordova.CallbackContext;
-import org.apache.cordova.CordovaPlugin;
-import org.json.JSONArray;
-import org.json.JSONException;
-import org.json.JSONObject;
+  import android.util.Log;
+  import android.webkit.CookieManager;
+  import android.webkit.CookieSyncManager;
+  import org.apache.cordova.CallbackContext;
+  import org.apache.cordova.CordovaPlugin;
+  import org.json.JSONArray;
+  import org.json.JSONException;
+  import org.json.JSONObject;
 
-import java.util.HashMap;
+  import java.util.HashMap;
 
-import cn.sharesdk.framework.Platform;
-import cn.sharesdk.framework.PlatformActionListener;
-import cn.sharesdk.framework.ShareSDK;
-import cn.sharesdk.wechat.friends.Wechat;
+  import cn.sharesdk.framework.Platform;
+  import cn.sharesdk.framework.PlatformActionListener;
+  import cn.sharesdk.framework.ShareSDK;
+  import cn.sharesdk.wechat.friends.Wechat;
 
 
 public class GetAjaxRequest extends CordovaPlugin{
-    @Override
-    public boolean execute(String action, JSONArray args, CallbackContext callbackContext) throws JSONException {
-        if ("setCookie".equals(action)){
-          String[] cookies = args.getString(0).split(";");
-          CookieManager cookieManager = CookieManager.getInstance();
-          cookieManager.setAcceptCookie(true);
-          for(int i = 0; i < cookies.length; i++) {
-            cookieManager.setCookie(args.getString(1), cookies[i]);
-          }
-          if (android.os.Build.VERSION.SDK_INT >= android.os.Build.VERSION_CODES.LOLLIPOP) {
-            cookieManager.flush();
-          } else {
-            CookieSyncManager.createInstance(this.cordova.getActivity());
-            CookieSyncManager.getInstance().sync();
-          }
-          callbackContext.success();
-        }else if (action.equals("login"))
-        {
-          login(callbackContext);
-          return true;
-        }
-        return  false;
-    }
+  @Override
+  public boolean execute(String action, JSONArray args, CallbackContext callbackContext) throws JSONException {
+    if ("setCookie".equals(action)){
+      String[] cookies = args.getString(0).split(";");
+      CookieManager cookieManager = CookieManager.getInstance();
+      cookieManager.setAcceptCookie(true);
+      for(int i = 0; i < cookies.length; i++) {
+        cookieManager.setCookie(args.getString(1), cookies[i]);
+      }
+      if (android.os.Build.VERSION.SDK_INT >= android.os.Build.VERSION_CODES.LOLLIPOP) {
+        cookieManager.flush();
+      } else {
+        CookieSyncManager.createInstance(this.cordova.getActivity());
+        CookieSyncManager.getInstance().sync();
+      }
+      callbackContext.success();
+    }else if (action.equals("login"))
+    {
+      login(callbackContext);
+    }else
+      return false;
+    return true;
+  }
 
   private void login(final CallbackContext paramCallbackContext)
   {
